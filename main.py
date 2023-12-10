d65ca74b02e16068647eae4a2710694896787b2b
CamronMyers <88673371+CamronMyers@users.noreply.github.com>
2023-12-10 16:02:28 -0600

    resize

--- main.py
+++ main.py
@@ -15,11 +15,11 @@ components.iframe("https://www.ventusky.com/?p=32.4;-97.4;5&l=wind-10m",width=11
 st.subheader("Live METAR-TAF Information")
 components.iframe("https://metar-taf.com/KDFW", width=1120, height=560)
 st.subheader("Flight Connections Lookup")
-components.iframe("https://www.flightconnections.com/",width=1120, height=560)
+components.iframe("https://www.flightconnections.com/",width=1120, height=700)
 st.subheader("Life ATC Radio (KDFW Arrivals)")
 components.iframe("http://s1-fmt2.liveatc.net/kdfw1_atis_arr?nocache=2023121021262584678")
 st.subheader("Live Marine Traffic Tracking")
-components.iframe("https://www.marinetraffic.com/en/ais/embed/zoom:8/centery:29/centerx:-95/maptype:4/shownames:false/mmsi:0/shipid:0/fleet:/fleet_id:/vtypes:/showmenu:/remember:false", width=1400, height=700)
+components.iframe("https://www.marinetraffic.com/en/ais/embed/zoom:8/centery:29/centerx:-95/maptype:4/shownames:false/mmsi:0/shipid:0/fleet:/fleet_id:/vtypes:/showmenu:/remember:false", width=1120, height=700)
 st.subheader("Live News Map - U.S.")
 components.iframe("https://usa.liveuamap.com/", width=1120,height=560)
 
